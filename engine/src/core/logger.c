0c1a2bc7cc71c359f7995501d7b3beadc10ea1cd
Von Mueller <vonjonmueller@gmail.com>
2024-01-14 23:44:35 -0800

    Add logger and assertions to core

---
+++ engine/src/core/logger.c
@@ -0,0 +1,43 @@
+#include "logger.h"
+
+// NOTE: temp
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+b8 init_logger() {
+    // TODO: Create log file
+    return TRUE;
+}
+
+void shutdown_logger() {
+    // TODO: Cleanup logging/write queued entries
+}
+
+void log_output(log_level level, const char * msg, ...) {
+    const char* level_strings[6] = {
+        "[FATAL]: ",
+        "[ERROR]: ",
+        "[WARN]: ",
+        "[INFO]:",
+        "[DEBUG]: ",
+        "[TRACE]: "
+    };
+
+    /* b8 is_error = level < 2; */
+
+    char in_msg[32000] = {0};
+
+    // To get around MS headers overide
+    __builtin_va_list arg_ptr;
+    va_start(arg_ptr, msg);
+    vsnprintf(in_msg, 32000, msg, arg_ptr);
+    va_end(arg_ptr);
+
+    char out_msg[32000];
+    sprintf(out_msg, "%s%s\n", level_strings[level], in_msg);
+
+    // NOTE: temp
+    printf("%s", out_msg);
+
+}
---
+++ engine/src/core/logger.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "../defines.h"
+
+#define LOG_WARN_ENABLED 1
+#define LOG_INFO_ENABLED 1
+#define LOG_DEBUG_ENABLED 1
+#define LOG_TRACE_ENABLED 1
+
+#if PRELEASE == 1
+#define LOG_DEBUG_ENABLED 0
+#define LOG_TRACE_ENABLED 0
+#endif
+
+typedef enum log_level {
+    LOG_LEVEL_FATAL = 0,
+    LOG_LEVEL_ERROR = 1,
+    LOG_LEVEL_WARN = 2,
+    LOG_LEVEL_INFO = 3,
+    LOG_LEVEL_DEBUG = 4,
+    LOG_LEVEL_TRACE = 5,
+} log_level;
+
+/**
+ * @brief Startup Logger
+ */
+b8 initialize_logging();
+
+/**
+ * @brief Shutdown and save logger queue
+ */
+void shutdown_logging();
+
+/**
+ * @brief Add message to logger
+ * @param level Log level
+ * @param msg Formatted string
+ */
+PAPI void log_output(log_level level, const char* msg, ...);
+
+#define PFATAL(msg, ...) log_output(LOG_LEVEL_FATAL, msg, ##__VA_ARGS__);
+
+#ifndef PERROR
+#define PERROR(msg, ...) log_output(LOG_LEVEL_ERROR, msg, ##__VA_ARGS__);
+#endif
+
+#if LOG_WARN_ENABLED == 1
+#define PWARN(msg, ...) log_output(LOG_LEVEL_WARN, msg, ##__VA_ARGS__);
+#else
+#define PWARN(msg, ...)
+#endif
+
+#if LOG_INFO_ENABLED == 1
+#define PINFO(msg, ...) log_output(LOG_LEVEL_INFO, msg, ##__VA_ARGS__);
+#else
+#define PINFO(msg, ...)
+#endif
+
+#if LOG_DEBUG_ENABLED == 1
+#define PDEBUG(msg, ...) log_output(LOG_LEVEL_DEBUG, msg, ##__VA_ARGS__);
+#else
+#define PDEBUG(msg, ...)
+#endif
+
+#if LOG_TRACE_ENABLED == 1
+#define PTRACE(msg, ...) log_output(LOG_LEVEL_TRACE, msg, ##__VA_ARGS__);
+#else
+#define PTRACE(msg, ...)
+#endif
