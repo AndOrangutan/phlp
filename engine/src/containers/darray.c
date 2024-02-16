484bc928f23fbd6302cd4621c6e9459a4735786f
Von Mueller <vonjonmueller@gmail.com>
2024-02-16 00:17:35 -0800

    Redo darray so it follows tutorial Was doing it in a "smarter" way, but got lost in the sauce. Will redo later

--- engine/src/containers/darray.c
+++ engine/src/containers/darray.c
@@ -17,73 +17,75 @@ void *_darray_create(u64 length, u64 stride) {
     return (void *)(new_array + DARRAY_FIELD_LENGTH);
 }
 
-void _darray_destroy(void **array) {
-    u64 *header = (u64 *)*array - DARRAY_FIELD_LENGTH;
+void _darray_destroy(void *array) {
+    u64 *header = (u64 *)array - DARRAY_FIELD_LENGTH;
     u64 header_size = DARRAY_FIELD_LENGTH * sizeof(u64);
     u64 total_size =
         header_size + header[DARRAY_CAPACITY] * header[DARRAY_STRIDE];
     pfree(header, total_size, MEMORY_TAG_DARRAY);
 }
 
-u64 _darray_field_get(void **array, u64 field) {
-    u64 *header = (u64 *)*array - DARRAY_FIELD_LENGTH;
+u64 _darray_field_get(void *array, u64 field) {
+    u64 *header = (u64 *)array - DARRAY_FIELD_LENGTH;
     return header[field];
 }
 
-void _darray_field_set(void **array, u64 field, u64 value) {
-    u64 *header = (u64 *)*array - DARRAY_FIELD_LENGTH;
+void _darray_field_set(void *array, u64 field, u64 value) {
+    u64 *header = (u64 *)array - DARRAY_FIELD_LENGTH;
     header[field] = value;
 }
 
-void _darray_resize(void **array) {
-    u64 length = darray_length(*array);
-    u64 stride = darray_stride(*array);
+void *_darray_resize(void *array) {
+    u64 length = darray_length(array);
+    u64 stride = darray_stride(array);
     void *temp =
         _darray_create((DARRAY_RESIZE_FACTOR * darray_capacity(array)), stride);
     pmemcpy(temp, array, length * stride);
 
     _darray_field_set(temp, DARRAY_LENGTH, length);
-    *array = temp;
+    _darray_destroy(array);
+    return temp;
 }
 
-void _darray_push(void **array, const void *value_ptr) {
-    u64 length = darray_length(*array);
-    u64 stride = darray_stride(*array);
+void *_darray_push(void *array, const void *value_ptr) {
+    u64 length = darray_length(array);
+    u64 stride = darray_stride(array);
 
-    if (length >= darray_capacity(*array)) {
-        _darray_resize(array);
+    if (length >= darray_capacity(array)) {
+        array = _darray_resize(array);
     }
 
-    u64 addr = (u64)*array;
+    u64 addr = (u64)array;
     addr += (length * stride);
     pmemcpy((void *)addr, value_ptr, stride);
     _darray_field_set(array, DARRAY_LENGTH, length + 1);
+    return array;
 }
 
-void _darray_pop(void **array, void *dest) {
-    u64 length = darray_length(*array);
-    u64 stride = darray_stride(*array);
-    u64 addr = (u64)*array;
+void _darray_pop(void *array, void *dest) {
+    u64 length = darray_length(array);
+    u64 stride = darray_stride(array);
+    u64 addr = (u64)array;
 
     addr += ((length - 1) * stride);
     pmemcpy(dest, (void *)addr, stride);
     _darray_field_set(array, DARRAY_LENGTH, length - 1);
 }
 
-void _darray_push_at(void **array, u64 index, void *value_ptr) {
-    u64 length = darray_length(*array);
-    u64 stride = darray_stride(*array);
+void _darray_push_at(void *array, u64 index, void *value_ptr) {
+    u64 length = darray_length(array);
+    u64 stride = darray_stride(array);
     if (index >= length) {
         PERROR(
             "Index outside the bounds of this array! Length: %i, index: %index",
             length, index);
-        return;
     }
-    if (length >= darray_capacity(*array)) {
+
+    if (length >= darray_capacity(array)) {
         _darray_resize(array);
     }
 
-    u64 addr = (u64)*array;
+    u64 addr = (u64)array;
 
     // Coppy rest outward
     if (index != length - 1) {
@@ -96,24 +98,26 @@ void _darray_push_at(void **array, u64 index, void *value_ptr) {
     _darray_field_set(array, DARRAY_LENGTH, length + 1);
 }
 
-void _darray_pop_at(void **array, u64 index, void *dest) {
-    u64 length = darray_length(*array);
-    u64 stride = darray_stride(*array);
+void *_darray_pop_at(void *array, u64 index, void *dest) {
+    u64 length = darray_length(array);
+    u64 stride = darray_stride(array);
     if (index >= length) {
         PERROR(
             "Index outside the bounds of this array! Length: %i, index: %index",
             length, index);
-        return;
+        return array;
     }
 
-    u64 addr = (u64)*array;
+    u64 addr = (u64)array;
     pmemcpy(dest, (void *)(addr + (index * stride)), stride);
 
     // Snip out the entry and copy rest inward
     if (index != length - 1) {
         pmemcpy((void *)(addr + (index * stride)),
-                (void *)(addr + ((index + 1) * stride)), stride * (length - (index + 1)));
+                (void *)(addr + ((index + 1) * stride)),
+                stride * (length - (index + 1)));
     }
 
     _darray_field_set(array, DARRAY_LENGTH, length - 1);
+    return array;
 }
